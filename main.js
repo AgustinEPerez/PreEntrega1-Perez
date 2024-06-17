1dbd6dc5550c47e588a6e2512b0539f39f50e963
Agustin Ezequiel Pérez <agusezeperez@gmail.com>
2024-06-17 18:46:44 -0300

    Agrego codigo Javascript.

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+function contadorInteractivo() {
+    let continuar = true;
+    while (continuar) {
+        let usuarioInput = prompt("Introduce un número para multiplicar por 2, o 'salir' para terminar el simulador.");
+        if (usuarioInput.toLowerCase() === 'salir') {
+            alert("Simulador terminado.");
+            continuar = false;
+        } else {
+            let numero = parseInt(usuarioInput);
+            if (!isNaN(numero)) {
+                let resultado = numero * 2;
+                alert("El resultado es: " + resultado);
+            } else {
+                alert("Por favor, introduce un número válido.");
+            }
+        }
+    }
+}
+
+contadorInteractivo();
 
