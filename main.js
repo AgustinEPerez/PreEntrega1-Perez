6414bb45f1cc5f327e0140b8c1aa826b28ed3e3c
Agustin Ezequiel Pérez <agusezeperez@gmail.com>
2024-07-25 20:49:18 -0300

    PreEntrega 3 - corregido

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-// Variables
 let elementos = [];
 let contador = 0;
 
-// Funciones
 function agregarElemento() {
     const elemento = {
         id: contador,
@@ -12,6 +10,7 @@ function agregarElemento() {
     elementos.push(elemento);
     contador++;
     actualizarLista();
+    guardarElementos();
 }
 
 function actualizarLista() {
@@ -25,58 +24,34 @@ function actualizarLista() {
     });
 }
 
+function borrarElementos() {
+    elementos = [];
+    contador = 0;
+    actualizarLista();
+    guardarElementos();
+}
+
+// Funcion para alternar el estado de activación de un elemento en la lista
 function toggleActivo(id) {
     const elemento = elementos.find((elemento) => elemento.id === parseInt(id));
     elemento.activo = !elemento.activo;
     actualizarLista();
 }
 
-function borrarElementos() {
-    elementos = [];
-    contador = 0;
-    actualizarLista();
+// Funciones para guardar elementos en el LocalStorage
+function guardarElementos() {
+    localStorage.setItem('elementos', JSON.stringify(elementos));
 }
 
+function cargarElementosGuardados() {
+    const elementosGuardados = JSON.parse(localStorage.getItem('elementos'));
+    elementosGuardados ? (elementos = elementosGuardados, contador = elementos.length, actualizarLista()) : null;
+}
 
 // Eventos
 document.getElementById('btn-agregar').addEventListener('click', agregarElemento);
 document.getElementById('btn-borrar').addEventListener('click', borrarElementos);
 
 document.addEventListener('click', (event) => {
-    if (event.target.tagName === 'LI') {
-        toggleActivo(event.target.id);
-    }
+    event.target.tagName === 'LI' ? toggleActivo(event.target.id) : null;
 });
-
-// Ciclos
-elementos.forEach((elemento) => {
-    console.log(elemento.nombre);
-});
-
-// Objetos y arrays
-const objeto = {
-    nombre: 'Objeto',
-    edad: 30
-};
-console.log(objeto.nombre);
-
-const array = [1, 2, 3, 4, 5];
-console.log(array.filter((numero) => numero > 3));
-
-// Funciones superiores
-const numeros = [1, 2, 3, 4, 5];
-console.log(numeros.find((numero) => numero > 3));
-console.log(numeros.forEach((numero) => console.log(numero)));
-console.log(numeros.filter((numero) => numero > 3));
-
-// Funciones avanzadas
-const esPar = (numero) => numero % 2 === 0;
-console.log([1, 2, 3, 4, 5].filter(esPar));
-
-const sumar = (...numeros) => numeros.reduce((a, b) => a + b, 0);
-console.log(sumar(1, 2, 3, 4, 5));
-
-// LocalStorage
-localStorage.setItem('elementos', JSON.stringify(elementos));
-const elementosGuardados = JSON.parse(localStorage.getItem('elementos'));
-console.log(elementosGuardados);
